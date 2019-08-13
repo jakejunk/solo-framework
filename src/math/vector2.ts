696a6d7dcb81b6bbb7c3898164284eef65e5bf1f
jakejunk <jake@jakejunker.com>
2019-08-12 22:06:21 -0600

    Vector2 and some tests

---
+++ src/math/vector2.ts
@@ -0,0 +1,109 @@
+export class Vector2
+{
+    x: number;
+    y: number;
+
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    static Clone(other: Vector2): Vector2
+    {
+        return new Vector2(other.x, other.y);
+    }
+
+    add(other: Vector2) 
+    {
+        this.x += other.x;
+        this.y += other.y;
+    }
+
+    sub(other: Vector2)
+    {
+        this.x -= other.x;
+        this.y -= other.y;
+    }
+
+    mul(other: Vector2)
+    {
+        this.x *= other.x;
+        this.y *= other.y;
+    }
+
+    div(other: Vector2)
+    {
+        this.x /= other.x;
+        this.y /= other.y;
+    }
+
+    scale(factor: number)
+    {
+        this.x *= factor;
+        this.y *= factor;
+    }
+
+    set(other: Vector2)
+    {
+        this.x = other.x;
+        this.y = other.y;
+    }
+
+    negate()
+    {
+        this.x = -this.x;
+        this.y = -this.y;
+    }
+
+    /**
+     * Converts this vector to a unit vector pointing in the same direction.
+     */
+    normalize()
+    {
+        const factor = 1 / this.getLength();
+        if (isFinite(factor))
+        {
+            this.x *= factor;
+            this.y *= factor;
+        }
+    }
+
+    dot(other: Vector2): number
+    {
+        return this.x * other.x + this.y * other.y
+    }
+
+    getAngle(): number
+    {
+        return Math.atan2(this.y, this.x);
+    }
+
+    getLength(): number
+    {
+        return Math.sqrt(this.getLengthSquared());
+    }
+
+    getLengthSquared(): number
+    {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    equals(other: Vector2): boolean
+    {
+        return this.x === other.x
+            && this.y === other.y;
+    }
+}
+
+export namespace Vector2
+{
+    /** (0, 0) */
+    export const ZERO: Readonly<Vector2> = new Vector2();
+
+    /** (1, 0) */
+    export const UNIT_X: Readonly<Vector2> = new Vector2(1, 0);
+
+    /** (0, 1) */
+    export const UNIT_Y: Readonly<Vector2> = new Vector2(0, 1);
+}
---
+++ tests/math/vector2.spec.ts
@@ -0,0 +1,133 @@
+import { Vector2 } from "../../src/math/vector2";
+import { expect } from "chai";
+import "mocha";
+
+describe("Vector2", () =>
+{
+    describe("scale()", () =>
+    {
+        it("equals (0, 0) when scaled by 0", () =>
+        {
+            const v = new Vector2(47, 7);
+            v.scale(0);
+
+            expect(v.getLength()).equals(0);
+        });
+
+        it("equals (0, 0) if Vector2.ZERO is scaled by anything", () =>
+        {
+            const v = Vector2.ZERO;
+            v.scale(768234);
+
+            expect(v.getLength()).equals(0);
+        });
+
+        it("remains unchanged when scaled by 1", () =>
+        {
+            const v = new Vector2(35, 675);
+            const originalLength = v.getLength();
+            
+            v.scale(1);
+
+            expect(v.getLength()).equals(originalLength);
+        });
+
+        it("doubles in length when scaled by 2", () =>
+        {
+            const v = new Vector2(45, 67);
+            const originalLength = v.getLength();
+            
+            v.scale(2);
+
+            expect(v.getLength()).equals(originalLength * 2);
+        });
+    });
+
+    describe("normalize()", () =>
+    {
+        it("does not change the length of Vector2.ZERO", () =>
+        {
+            const v = Vector2.ZERO;
+            v.normalize();
+
+            expect(v.getLength()).equals(0);
+        });
+
+        it("results in length ~= 1, for any non-zero vector", () =>
+        {
+            const v = new Vector2(0.1, 0.1);
+            v.normalize();
+
+            expect(v.getLength()).within(0.9999999999999999, 1.000000000000001);
+        });
+    });
+
+    describe("dot()", () =>
+    {
+        it("is the sum of the products of each component", () =>
+        {
+            const x1 = 25;
+            const y1 = 75;
+            const x2 = 3;
+            const y2 = 5;
+            const v1 = new Vector2(x1, y1);
+            const v2 = new Vector2(x2, y2);
+
+            expect(v1.dot(v2)).equals(x1 * x2 + y1 * y2);
+        });
+
+        it("equals getLengthSquared() when performed with itself", () =>
+        {
+            const v = new Vector2(354, 576);
+
+            expect(v.dot(v)).equals(v.getLengthSquared());
+        });
+
+        it("is commutative", () =>
+        {
+            const v1 = new Vector2(1, 2);
+            const v2 = new Vector2(3, 4);
+
+            expect(v1.dot(v2)).equals(v2.dot(v1));
+        });
+
+        it("equals 0 when performed with orthogonal vectors", () =>
+        {
+            const v1 = new Vector2(2, 2);
+            const v2 = new Vector2(-2, 2);
+
+            expect(v1.dot(v2)).equals(0);
+        });
+    });
+
+    describe("getLength()", () =>
+    {
+        it("equals getLengthSquared() for Vector2.ZERO", () =>
+        {
+            const v = Vector2.ZERO;
+
+            expect(v.getLength()).equals(v.getLengthSquared());
+        });
+
+        it("equals getLengthSquared() if getLength() == 1", () =>
+        {
+            const v = Vector2.UNIT_X;
+
+            expect(v.getLength()).equals(v.getLengthSquared());
+        });
+
+        it("is < getLengthSquared() if getLength() > 1", () =>
+        {
+            const v = new Vector2(1, 1);
+
+            expect(v.getLength()).lessThan(v.getLengthSquared());
+        });
+
+        it("is > getLengthSquared() if getLength() < 1", () =>
+        {
+            const v = new Vector2(0.1, 0.1);
+
+            expect(v.getLength()).greaterThan(v.getLengthSquared());
+        });
+    });
+});
