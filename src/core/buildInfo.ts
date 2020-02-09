554da0acb79fa0e8f75d01a8c15c2614b74bb95a
jakejunk <jake@jakejunker.com>
2020-02-09 15:13:36 -0700

    Fancy warning

--- src/core/buildInfo.ts
+++ src/core/buildInfo.ts
@@ -1,1 +1,5 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This value is overwritten during the build process. Do not change it.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 export const FrameworkVersion = "_INJECT_BUILD_VERSION_HERE_";
