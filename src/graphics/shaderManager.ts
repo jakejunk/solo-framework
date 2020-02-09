0517504b13e93f1949be1f646f9f7c8f83af275f
jakejunk <jake@jakejunker.com>
2020-02-08 20:16:46 -0700

    Some comments

--- src/graphics/shaderManager.ts
+++ src/graphics/shaderManager.ts
@@ -5,8 +5,14 @@ export type UniformLocation = WebGLUniformLocation;
 
 export interface ShaderManager
 {
+    /**
+     * Creates a new `ShaderProgram` from a vertex and fragment shader, written in GLSL.
+     */
     createShaderProgram(vShaderSrc: string, fShaderSrc: string): Result<ShaderProgram, Error>;
 
+    /**
+     * Binds the provided program for rendering.
+     */
     useProgram(program: ShaderProgram): void;
 
     /**
