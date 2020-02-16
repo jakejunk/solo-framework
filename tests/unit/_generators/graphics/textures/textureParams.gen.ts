bcc251ad56b4635c3d78c278fa5e7e805c9d167d
jakejunk <jake@jakejunker.com>
2020-02-15 18:52:35 -0700

    More texture tests

---
+++ tests/unit/_generators/graphics/textures/textureParams.gen.ts
@@ -0,0 +1,74 @@
+import { TextureMagFilter } from "../../../../../src/graphics/constants/textureMagFilter";
+import { TextureMinFilter } from "../../../../../src/graphics/constants/textureMinFilter";
+import { TextureParams } from "../../../../../src/graphics/textures/textureParams";
+import { TextureWrap } from "../../../../../src/graphics/constants/textureWrap";
+import { Writeable } from "../../../_helpers/writeable";
+
+export function TextureParamsAllDefault(): TextureParams
+{
+    return {
+        handle: 0,
+        width: 0,
+        height: 0,
+        minFilter: TextureMinFilter.LINEAR,
+        magFilter: TextureMagFilter.LINEAR,
+        wrapS: TextureWrap.CLAMP_TO_EDGE,
+        wrapT: TextureWrap.CLAMP_TO_EDGE
+    };
+};
+
+export function TextureParamsWithUri(uri: string): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.uri = uri;
+
+    return params;
+}
+
+export function TextureParamsWithWidth(width: number): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.width = width;
+
+    return params;
+}
+
+export function TextureParamsWithHeight(height: number): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.height = height;
+
+    return params;
+}
+
+export function TextureParamsWithMinFilter(minFilter: TextureMinFilter): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.minFilter = minFilter;
+
+    return params;
+}
+
+export function TextureParamsWithMagFilter(magFilter: TextureMagFilter): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.magFilter = magFilter;
+
+    return params;
+}
+
+export function TextureParamsWithWrapS(wrapS: TextureWrap): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.wrapS = wrapS;
+
+    return params;
+}
+
+export function TextureParamsWithWrapT(wrapT: TextureWrap): TextureParams
+{
+    const params: Writeable<TextureParams> = TextureParamsAllDefault();
+    params.wrapT = wrapT;
+
+    return params;
+}
--- tests/unit/graphics/textures/texture2d.spec.ts
+++ tests/unit/graphics/textures/texture2d.spec.ts
@@ -1,11 +1,98 @@
 import { expect } from "chai";
+import { Texture2D } from "../../../../src/graphics/textures/texture2d";
 import { TextureMagFilter } from "../../../../src/graphics/constants/textureMagFilter";
 import { TextureMinFilter } from "../../../../src/graphics/constants/textureMinFilter";
 import { TextureNoHandle } from "../../_generators/graphics/textures/texture2d.gen";
+import {
+    TextureParamsWithHeight,
+    TextureParamsWithMagFilter,
+    TextureParamsWithMinFilter,
+    TextureParamsWithUri,
+    TextureParamsWithWidth,
+    TextureParamsWithWrapS,
+    TextureParamsWithWrapT
+    } from "../../_generators/graphics/textures/textureParams.gen";
 import { TextureWrap } from "../../../../src/graphics/constants/textureWrap";
 
 describe("ShaderProgram", () =>
 {
+    describe("getUri()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const uri = "/home/";
+            const texture = new Texture2D(undefined, TextureParamsWithUri(uri));
+
+            expect(texture.getUri()).to.equal(uri);
+        });
+    });
+
+    describe("getWidth()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const width = 200;
+            const texture = new Texture2D(undefined, TextureParamsWithWidth(width));
+
+            expect(texture.getWidth()).to.equal(width);
+        });
+    });
+
+    describe("getHeight()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const height = 400;
+            const texture = new Texture2D(undefined, TextureParamsWithHeight(height));
+
+            expect(texture.getHeight()).to.equal(height);
+        });
+    });
+
+    describe("getMinFilter()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const minFilter = TextureMinFilter.LINEAR_MIPMAP_NEAREST;
+            const texture = new Texture2D(undefined, TextureParamsWithMinFilter(minFilter));
+
+            expect(texture.getMinFilter()).to.equal(minFilter);
+        });
+    });
+
+    describe("getMagFilter()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const magFilter = TextureMagFilter.NEAREST;
+            const texture = new Texture2D(undefined, TextureParamsWithMagFilter(magFilter));
+
+            expect(texture.getMagFilter()).to.equal(magFilter);
+        });
+    });
+
+    describe("getWrapS()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const wrapS = TextureWrap.REPEAT;
+            const texture = new Texture2D(undefined, TextureParamsWithWrapS(wrapS));
+
+            expect(texture.getWrapS()).to.equal(wrapS);
+        });
+    });
+
+    describe("getWrapT()", () =>
+    {
+        it("returns correct value", () =>
+        {
+            const wrapT = TextureWrap.REPEAT;
+            const texture = new Texture2D(undefined, TextureParamsWithWrapT(wrapT));
+
+            expect(texture.getWrapT()).to.equal(wrapT);
+        });
+    });
+
     describe("setFilter()", () =>
     {
         it("roundtrips with getMinFilter()", () =>
@@ -13,7 +100,7 @@ describe("ShaderProgram", () =>
             const texture = TextureNoHandle;
             const filter = TextureMinFilter.LINEAR;
 
-            texture.setFilter(filter, TextureMagFilter.NEAREST)
+            texture.setFilter(filter, TextureMagFilter.NEAREST);
 
             expect(texture.getMinFilter()).to.equal(filter);
         });
@@ -23,7 +110,7 @@ describe("ShaderProgram", () =>
             const texture = TextureNoHandle;
             const filter = TextureMagFilter.NEAREST;
 
-            texture.setFilter(TextureMinFilter.LINEAR, filter)
+            texture.setFilter(TextureMinFilter.LINEAR, filter);
 
             expect(texture.getMagFilter()).to.equal(filter);
         });
